2f6c7b1062e3f1486c4e7f53e96d8e0597975af5
PedrinDev <pedro.silva25@estudante.ifms.edu.br>
2023-05-27 02:25:27 -0400

    ReMaking the movement and the tileset

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,17 +1,36 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    private TankSO tankSO;
+    [SerializeField] TankSO tankSO;
 
     [Header("Movement")]
-    private TankMovement tankMovement;
+    private Vector3 initialPosition;
+
+    [Header("Gun")]
+    public Transform gunTransform;
+    private Quaternion initialGunRotation;
+    private float rotationLimiter = 90f;
 
     private void Awake()
     {
-        tankMovement = FindObjectOfType<TankMovement>();
+        initialPosition = transform.position;
+        initialGunRotation = gunTransform.rotation;
     }
 
+    private void Update()
+    {
+        //________________Turret Follow________________//
+        Vector3 mousePosition = Input.mousePosition;
+        Vector3 mouseWorldPosition = Camera.main.ScreenToWorldPoint(mousePosition);
+        Vector3 direction = mouseWorldPosition - gunTransform.position;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        angle = Mathf.Clamp(angle, -rotationLimiter, rotationLimiter);
+        gunTransform.rotation = initialGunRotation * Quaternion.AngleAxis(angle, Vector3.forward);
+    }
+
+    private void OnBecameInvisible()
+    {
+        transform.position = initialPosition;
+    }
 }
