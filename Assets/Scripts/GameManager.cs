59ec6c3bd5aa569eb45e78430a6e7daef7b204da
MrWizardry <pedro.silva25@estudante.ifms.edu.br>
2023-05-31 16:22:58 -0300

    shooting and parallax

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -12,6 +12,14 @@ public class GameManager : MonoBehaviour
     private Quaternion initialGunRotation;
     private float rotationLimiter = 90f;
 
+    [Header("Shoot")]
+    [SerializeField] private GameObject bulletPrefab;
+    [SerializeField] private Transform bulletSpawn;
+    [SerializeField] private float bulletSpeed = 10f;
+    [SerializeField] private float shootDelay = 0f;
+    [SerializeField] private float bulletLifespan = 5f; // Time in seconds
+    private float shootCooldown = 0f;
+
     private void Awake()
     {
         initialPosition = transform.position;
@@ -27,10 +35,32 @@ public class GameManager : MonoBehaviour
         float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
         angle = Mathf.Clamp(angle, -rotationLimiter, rotationLimiter);
         gunTransform.rotation = initialGunRotation * Quaternion.AngleAxis(angle, Vector3.forward);
+
+        //________________Shooting________________//
+        if (shootCooldown > 0)
+        {
+            shootCooldown -= Time.deltaTime;
+        }
+        else
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                Shoot();
+                shootCooldown = shootDelay;
+            }
+        }
     }
 
     private void OnBecameInvisible()
     {
         transform.position = initialPosition;
+    }
+
+    private void Shoot()
+    {
+        GameObject bullet = Instantiate(bulletPrefab, bulletSpawn.position, bulletSpawn.rotation);
+        Rigidbody2D bulletRigidbody = bullet.GetComponent<Rigidbody2D>();
+        bulletRigidbody.velocity = bulletSpawn.right * bulletSpeed;
+        Destroy(bullet, bulletLifespan);
     }
 }
